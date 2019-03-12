6d529d1ed3bee95ed9b00a1636d111bf76d8face
Alexander Alemayhu <alexander@alemayhu.com>
2019-03-12 22:34:26 +0100

    Add underline to the navigation
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -6,9 +6,13 @@
 				<g-link class="site-link" to="/">{{ $static.metaData.siteName }}</g-link>
 			</strong>
 			<nav class="nav">
-				<g-link class="nav__link" to="/about">About</g-link>
-				<g-link class="nav__link" to="/apps">Apps</g-link>
-				<g-link class="nav__link" to="/contact">Contact</g-link>
+				<g-link v-bind:class="[isAbout ? 'active-link' : 'nav-link']" to="/about">About</g-link>
+				<g-link v-bind:class="[isApps ? 'active-link' : 'nav-link']" class="nav__link" to="/apps">Apps</g-link>
+				<g-link
+					v-bind:class="[isContact ? 'active-link' : 'nav-link']"
+					class="nav__link"
+					to="/contact"
+				>Contact</g-link>
 			</nav>
 		</header>
 		<slot/>
@@ -47,7 +51,15 @@ body {
 	height: 80px;
 }
 
-a.nav__link {
+.active-link {
+	border-bottom: 0.4px solid black;
+	margin-left: 20px;
+	font-weight: bold;
+	color: black;
+	text-decoration: none;
+}
+
+.nav-link {
 	margin-left: 20px;
 	font-weight: bold;
 	color: black;
@@ -59,8 +71,19 @@ a.site-link {
 	color: black;
 	text-decoration: none;
 }
-
-a.nav__link:active {
-	border-bottom: 0.4px solid black;
-}
 </style>
+
+<script>
+export default {
+	data: function() {
+		return { isAbout: true, isApps: false, isContact: false };
+	},
+	mounted: function() {
+		const path = this.$route.path;
+
+		this.isAbout = path.includes("/about");
+		this.isApps = path.includes("/apps");
+		this.isContact = path.includes("/contact");
+	}
+};
+</script>

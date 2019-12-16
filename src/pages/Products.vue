85e623857f6a3250e81bfa0b9fa0e915a0729878
Alexander Alemayhu <alexander@alemayhu.com>
2019-12-16 14:39:47 +0100

    add email sign up form

--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -9,8 +9,14 @@
 <script>
 export default {
   metaInfo: {
-    title: "About us"
-  }
+    title: "Products | alemayhu.com"
+  },
+	mounted() {
+		let emailSignup = document.createElement('script')
+		emailSignup.setAttribute('src', 'https://alemayhu.ck.page/8357068a71/index.js')
+		emailSignup.setAttribute('data-uid', '8357068a71')
+		document.body.appendChild(emailSignup)
+	}
 };
 </script>
 
@@ -20,5 +26,9 @@ export default {
 }
 .contact-link {
   color: black;
+}
+
+form {
+	margin: 10rem;
 }
 </style>

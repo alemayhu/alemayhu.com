73ba765e1be91d0e783f151af79dcc41aa946f9f
Alexander Alemayhu <alexander@alemayhu.com>
2019-12-21 15:35:38 +0100

    add YouTube embed

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,7 @@
 <template>
   <Layout>
-  <p>This site is still under construction</p>
+  <p>This site is still under construction, in the meantime checkout my YouTube channel</p>
+  <div class="g-ytsubscribe" data-channelid="UCumJa0eRO9_xtEsoAt3UCkQ" data-layout="full" data-count="hidden"></div>
   </Layout>
 </template>
 
